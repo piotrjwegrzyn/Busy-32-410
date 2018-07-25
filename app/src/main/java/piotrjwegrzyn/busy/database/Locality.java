2ac5c5d23fa4f46ddbee47498fda3299685fd884
piotrjwegrzyn <piotrwegrzyn@protonmail.com>
2018-07-25 22:28:16 +0200

    Podwaliny pod bazę danych nowego typu

--- app/src/main/java/piotrjwegrzyn/busy/database/Locality.java
+++ app/src/main/java/piotrjwegrzyn/busy/database/Locality.java
@@ -5,10 +5,12 @@ import android.arch.persistence.room.PrimaryKey;
 
 @Entity(tableName = "Localities")
 public class Locality {
+    
     @PrimaryKey(autoGenerate = true)
     public int l_id;
 
     String l_name;
     String l_gmina;
     String l_powiat;
+
 }
